8fd258aaeb140865882f2ae62e37a68a85454602
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-08-16 17:16:57 +0530

    Done

--- ap.js
+++ ap.js
@@ -1,16 +1,72 @@
-const stockUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=2C8P70FPKFBZGMCM';
 
-fetch(stockUrl)
-    .then((res) => {
-        return res.json()
-    })
-    .then((data) => {
-        const Ob = Object.values(data)[1]
+const stockLevel = document.querySelector('#stock-symbol');
+const stockQuantity = document.querySelector('#quantity');
+const purchsePrice = document.querySelector('#purchase-price');
 
-        // daily prices of a stock
-        const dailyPrices = Object.values(Ob)[0]
+const form = document.forms[0];
 
-        //daily current prices
-        const dailyCurrentPrice = Object.values(dailyPrices)[3]
-        console.log(dailyCurrentPrice)
-    })
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const stockLevelValue = stockLevel.value.toUpperCase();
+
+    const stockUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockLevelValue}&apikey=2C8P70FPKFBZGMCM`;
+
+    // const url = `https://api.tiingo.com/tiingo/daily/${stockLevelValue}/prices`;
+
+    fetch(stockUrl)
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            const Ob = Object.values(data)[1]
+            // daily prices of a stock
+            const dailyPrices = Object.values(Ob)[0]
+            //daily current prices
+            const dailyCurrentPrice = Number(Object.values(dailyPrices)[3])
+            // console.log(dailyCurrentPrice)
+            getProfitLoss(dailyCurrentPrice)
+        })
+
+})
+
+
+// find profit and loss
+
+function getProfitLoss(price) {
+
+    const currentPrice = price
+    const stockQuanityValue = Number(stockQuantity.value)
+    const purchasePriceValue = Number(purchsePrice.value)
+
+    // console.log(currentPrice, stockQuanityValue, purchasePriceValue)
+
+    if (currentPrice > 0 && stockQuanityValue > 0 && purchasePriceValue > 0) {
+
+        // find loss
+        if (purchasePriceValue > currentPrice) {
+            //total Loss
+            const totalLoss = ((purchasePriceValue - currentPrice) * stockQuanityValue).toFixed(2);
+            //loss in percentage
+            const lossPerce = (((purchasePriceValue - currentPrice) * 100) / purchasePriceValue).toFixed(2);
+
+            message(`You lost ${lossPerce}%, and Your total loss is ₹${totalLoss}`)
+
+        } else {
+            //total profit in cash
+            const totalProfit = ((currentPrice - purchasePriceValue) * stockQuanityValue).toFixed(2);
+            //profit in percentage
+            const profitPer = (((currentPrice - purchasePriceValue) * 100) / purchasePriceValue).toFixed(2);
+
+
+            message(`You gain ${profitPer}%, and Your total profit is ₹${totalProfit}`)
+        }
+    }
+
+}
+
+function message(str) {
+    var showMessage = ''
+    showMessage = ` <div class="loss-info"><p>${str}</p></div>`
+    document.querySelector(".output").innerHTML = showMessage;
+}
